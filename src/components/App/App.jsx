a5e315d4fb0959d51796aad2abc3f8bd71c5168d
Yunosuke Sato <yunosukesato04@gmail.com>
2023-03-25 11:34:47 -0700

    fixed some bugs

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,8 +3,6 @@ import "./App.css";
 import Header from "../Header/Header";
 import Translate from "../Translate/Translate";
 import TextBox from "../Translate/TextBox/TextBox";
-import { useReducer } from "react";
-import { useSelector } from "react-redux";
 
 export const ACTIONS = {
   SET_LANG: "set-lang",
@@ -12,16 +10,7 @@ export const ACTIONS = {
   SET_SENTENCE: "set-sentence",
 };
 
-function reducer(state, { type, payload }) {
-  switch (type) {
-    case ACTIONS.SET_LANG:
-    case ACTIONS.CHANGE_MODE:
-    case ACTIONS.SET_SENTENCE:
-  }
-}
-
 function App() {
-
   return (
     <>
       <Header />
