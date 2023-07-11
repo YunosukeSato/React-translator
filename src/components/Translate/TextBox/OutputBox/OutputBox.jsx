8f9db3ccad8d366450f7c127c358f496b63457b4
Yunosuke Sato <yunosukesato04@gmail.com>
2023-07-10 20:15:59 -0700

    modified OutputBox

--- src/components/Translate/TextBox/OutputBox/OutputBox.jsx
+++ src/components/Translate/TextBox/OutputBox/OutputBox.jsx
@@ -1,13 +1,26 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { doTranslate, getAnswer } from "../../../../store/store";
+import { doTranslate, getAnswer, translationRequest } from "../../../../store/store";
 
 export default function OutputBox() {
-  const result = useSelector((state) => state.reducer.result);
+  const state = useSelector((state) => state.reducer);
+
+  const result = state.result;
 
   const [newResult, setResult] = useState(result);
+  const [translating, setTranslating] = useState(false);
+  // console.log(result);
 
   const dispatch = useDispatch();
+
+  const handleTranslate = (boolean) => {
+    setTranslating(boolean);
+  };
+
+  useEffect(() => {
+    setResult(result);
+  }, [result])
 
   return (
     <>
@@ -18,16 +31,21 @@ export default function OutputBox() {
       <textarea
         readOnly
         className="resize-none block p-9 w-1/2 h-96 text-3xl text-gray-900 bg-gray-50 rounded-lg border border-sky-200 focus:rind-blue-200"
-        value={newResult}
-      ></textarea>
+        value={translating ? 'Translating...' : newResult}
+      >
+      </textarea>
       <button
         className="w-32 h-10 rounded-xl flex items-center justify-center bg-sky-200 mx-2.5 hover:bg-sky-400"
         onClick={() => {
           // There should be better way to handle result not using setTimeout function...
-          dispatch(doTranslate());
-          setTimeout(() => {
-            setResult(getAnswer());
-          }, 3000);
+          handleTranslate(true);
+          // dispatch(doTranslate());
+          // setTimeout(() => {
+          //   handleTranslate(false);
+          //   setResult(getAnswer());
+          // }, 3000);
+          dispatch(translationRequest(state))
+          handleTranslate(false);
         }}
       >
         Translate
