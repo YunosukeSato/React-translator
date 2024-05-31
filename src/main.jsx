bf6da4192abc12dcd10e9a6828bb88be5f04297b
Yunosuke Sato <yunosukesato04@gmail.com>
2024-05-31 18:44:45 +0900

    Switch to configureStore from createStore

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 // Internal import
-import App from "./components/App/App";
-import "./index.css";
-import storeApp from "./store/store";
+import App from './components/App/App'
+import './index.css'
+import translationReducer from './store/store'
 // External import
-import { createStore } from "redux";
-import { Provider } from "react-redux";
+import { Provider } from 'react-redux'
+import { applyMiddleware, configureStore } from '@reduxjs/toolkit'
+import { thunk } from 'redux-thunk'
 
-const store = createStore(storeApp);
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+// const store = configureStore(storeApp, applyMiddleware(thunk))
+const store = configureStore({
+    reducer: {
+      translation: translationReducer,
+    },
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(thunk),
+})
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
+)
